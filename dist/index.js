d078d791245210c0720bc6988b1e5520f916886a
Igor Abreu <igor.abreu.reis@gmail.com>
2019-05-28 00:43:03 -0300

    feat: add nodemon + express

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var express = require("express");
+var app = express();
+
+app.listen(3000, function () {
+  console.log("server running on port 3000");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
