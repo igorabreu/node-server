56fa5f7f0e76f94973f8e61f8a5d13ed8be7f331
Igor Abreu <igor.abreu.reis@gmail.com>
2019-05-28 00:54:25 -0300

    feat: update babel

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require("express")
 const app = express()
 
 app.listen(3000, () => {
-  console.log("server running on port 3000")
+  console.log("server running on port 3001")
 })
 
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  res.send("Hello World1")
 })
